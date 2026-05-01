a31cf271eaca27a871590d2f18086e89383abe6d
Rmf-RubenRiofrio <162364734+Rmf-RubenRiofrio@users.noreply.github.com>
2026-05-01 09:39:02 -0400

    Add LoginPage POM and refactor LoginTest
    Introduce a LoginPage page object (src/test/java/com/ruben/pages/LoginPage.java) encapsulating login locators and interactions (enterUsername, enterPassword, clickLoginButton, getFlashMessage). Refactor LoginTest to use the new page object, remove duplicated locators/imports, and simplify the success assertion. Also add .idea/vcs.xml IDE VCS mapping file.

---
+++ src/test/java/com/ruben/pages/LoginPage.java
@@ -0,0 +1,36 @@
+package com.ruben.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginPage {
+    private final WebDriver driver;
+
+    private final By usernameInput = By.id("username");
+    private final By passwordInput = By.id("password");
+    private final By loginButton = By.cssSelector("button[type='submit']");
+    private final By flashMessage = By.id("flash");
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void enterUsername(String username) {
+        driver.findElement(usernameInput).sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        driver.findElement(passwordInput).sendKeys(password);
+    }
+
+    public void clickLoginButton() {
+        driver.findElement(loginButton).click();
+    }
+
+    public String getFlashMessage() {
+        WebElement message = driver.findElement(flashMessage);
+        return message.getText();
+    }
+}
--- src/test/java/com/ruben/tests/LoginTest.java
+++ src/test/java/com/ruben/tests/LoginTest.java
@@ -1,42 +1,33 @@
 package com.ruben.tests;
 
 import com.ruben.framework.core.BaseTest;
+import com.ruben.pages.LoginPage;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LoginTest extends BaseTest {
 
     @Test
     void validLogin_shouldShowSuccessMessage() {
-        String testName = "validLogin_shouldShowSuccessMessage";
-
         step("Open login page");
         driver.get("https://the-internet.herokuapp.com/login");
 
+        LoginPage loginPage = new LoginPage(driver);
+
         step("Enter username");
-        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        loginPage.enterUsername("tomsmith");
 
         step("Enter password");
-        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
-        //driver.findElement(By.id("password")).sendKeys("WrongPassword");
+        loginPage.enterPassword("SuperSecretPassword!");
 
         step("Click login button");
-        driver.findElement(By.cssSelector("button[type='submit']")).click();
+        loginPage.clickLoginButton();
 
         step("Verify success message");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        var flash = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("flash")));
-
-        assertTrue(flash.getText().toLowerCase().contains("you logged into a secure area"),
-                "Expected success message, But got: " + flash.getText());
+        assertTrue(
+                loginPage.getFlashMessage().toLowerCase().contains("you logged into a secure area"),
+                "Expected success message, but got: " + loginPage.getFlashMessage()
+        );
     }
 }
-
-
